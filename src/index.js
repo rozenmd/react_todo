69b77c7c8ae5b1c7b1a410e57e4f301bd9012ade
Max Rozen <rozenmd@gmail.com>
2017-07-17 22:02:37 +1000

    Implement up to Episode 19. of Getting started with Redux

---
+++ src/index.js
@@ -0,0 +1,154 @@
+import React, {Component} from 'react';
+import {combineReducers, createStore} from 'redux';
+import ReactDOM from 'react-dom';
+
+
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state
+            }
+            return {
+                ...state,
+                completed: !state.completed
+
+            };
+        default:
+            return state;
+    }
+};
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(t =>
+                todo(t, action)
+            );
+        default:
+            return state;
+    }
+};
+
+const visibilityFilter = (state = 'SHOW_ALL',
+                          action) => {
+    switch (action.type) {
+        case 'SET_VISBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
+
+const store = createStore(todoApp);
+
+const FilterLink = ({
+                        filter,
+                        children
+                    }) => {
+    return (
+        <a href="#"
+           onClick={e => {
+               e.preventDefault();
+               store.dispatch({
+                   type: 'SET_VISIBILITY_FILTER',
+                   filter
+               });
+           }}
+
+        >
+            {children}
+        </a>
+    )
+};
+
+let nextTodoId = 0;
+class TodoApp extends Component {
+    render() {
+        return (
+            <div>
+                <input ref={node => {
+                    this.input = node;
+                }}/>
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        text: this.input.value,
+                        id: nextTodoId++
+                    });
+                    this.input.value = '';
+                }}> Add Todo
+                </button>
+                <ul>
+                    {this.props.todos.map(todo =>
+                        <li key={todo.id}
+                            onClick={() => {
+                                store.dispatch({
+                                    type: 'TOGGLE_TODO',
+                                    id: todo.id
+                                });
+                            }}
+                            style={{
+                                textDecoration: todo.completed ?
+                                    'line-through' :
+                                    'none'
+                            }}>
+                            {todo.text}
+                        </li>
+                    )}
+                </ul>
+                <p>
+                    Show:
+                    {' '}
+                    <FilterLink
+                        filter='SHOW_ALL'
+                    >
+                        All
+                    </FilterLink>
+                    {' '}
+                    <FilterLink
+                        filter='SHOW_ACTIVE'
+                    >
+                        Active
+                    </FilterLink>
+                    {' '}
+                    <FilterLink
+                        filter="'SHOW_COMPLETED"
+                    >
+                        Completed
+                    </FilterLink>
+                </p>
+            </div>
+        );
+    }
+}
+
+const render = () => {
+    ReactDOM.render(
+        <TodoApp
+            todos={store.getState().todos}
+        />,
+        document.getElementById('root')
+    );
+};
+
+
+store.subscribe(render);
+render();
+
